a81ed78bd70a6fbe4caed0079b712c454111a777
hugo miranda <hugo.miranda.souza@gmail.com>
2022-09-22 20:17:40 +0000

    merge-sql-files-action

---
+++ sql/teste.sql
@@ -0,0 +1,27 @@
+---------SCRIPT FROM a.sql------------
+-- SCRIPT A
+BEGIN;
+
+-- SQL SCRIPT
+
+COMMIT;
+-- SCRIPT A
+
+---------SCRIPT FROM b.sql------------
+-- SCRIPT B
+BEGIN;
+
+-- SQL SCRIPT
+
+COMMIT;
+-- SCRIPT B
+
+---------SCRIPT FROM c.sql------------
+-- SCRIPT C
+BEGIN;
+
+-- SQL SCRIPT
+
+COMMIT;
+-- SCRIPT C
+
